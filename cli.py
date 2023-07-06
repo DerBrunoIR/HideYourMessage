8e8ff840e7ab5d752fdf6a1a3fa6aeb22298d4c5
DerBrunoIR <bruno.ignaz.rothschild@gmail.com>
2023-07-06 23:31:33 +0200

    tested application

--- cli.py
+++ cli.py
@@ -138,7 +138,7 @@ if __name__ == "__main__":
     parser.add_argument("--debug", 
                         dest="debug", 
                         action="store_true", 
-                        default=True, # False, 
+                        default=False, 
                         required=False, 
                         help="enable debug logs into stderr"
                         )
